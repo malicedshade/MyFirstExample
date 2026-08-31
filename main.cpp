0d34fb53a750ac9072aba3d18a48e8978fce652d
Alice Roldan <captianboingy@gmail.com>
2026-08-31 12:38:58 -0700

    Changed int types to int32_t, added range message.

--- main.cpp
+++ main.cpp
@@ -8,9 +8,10 @@ using std::cout;
 int main()
 {
     cout << "THE FIRST EXAMPLE MATH DISPLAY!\n";
+    cout << "(Limited to whole numbers between -2,147,483,648 and 2,147,483,647)\n";
     cout << "Hi, please enter two whole numbers: ";
 
-    int x,y;
+    int32_t x,y;
 
     cin >> x >> y;
     cout << "Addition: " << x + y << endl;
@@ -18,8 +19,8 @@ int main()
     cout << "Multiplication: " << x * y << endl;
     cout << "Division: " << x / y << endl;
     cout << "Remainder: " << x % y << endl;
-    cout << "Square Root: " << sqrt(x) << endl;
-    cout << "Square: " << pow(x, y) << endl;
+    cout << "Square Root: " << sqrt((float)x) << endl;
+    cout << "Square: " << pow((float)x, (float)y) << endl;
 
     return 0;
 }
